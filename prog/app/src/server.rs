7e828f4c8415dd8dafa29c04c2a6bbcb0c1000dd
takejuhi <shoma.takehara.7c@stu.hosei.ac.jp>
2023-12-11 00:20:01 +0900

    update

--- prog/app/src/server.rs
+++ prog/app/src/server.rs
@@ -6,11 +6,28 @@ use crate::{ecall, User};
 pub struct Server;
 
 impl Server {
-    pub const fn new() -> Self {
-        unimplemented!("start server");
+    pub const fn new() -> Result<Self> {
+        let listener = TcpListener::bind("localhost:5432")?;
+        Ok(listener)
     }
     pub fn wait(&mut self) -> Result<()> {
-        unimplemented!("wait for connection");
+        let stream = listener.accept()?;
+        println!("addr: {}", stream.1);
+
+        let mut stream = stream.0;
+        let mut buf = [0u8; 1024];
+        let size = stream.read(&mut buf)?;
+
+        if size = 0 {
+            continue;
+        }
+
+        let data = String::from_utf8(buf.to_vec())?;
+        println!("{data}");
+
+        if let Err(e) = stream.write(b"hello") {
+            eprintln!("fail to write");
+        }
     }
     pub unsafe fn verify(
         &self,
